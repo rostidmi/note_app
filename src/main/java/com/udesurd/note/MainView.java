935fae93a62fa17b210fd1da47a6a669c6665481
Rostislav Dmitriev <udesurd@gmail.com>
2022-06-03 16:38:58 +0300

    upd

--- src/main/java/com/udesurd/note/MainView.java
+++ src/main/java/com/udesurd/note/MainView.java
@@ -1,18 +1,33 @@
 package com.udesurd.note;
 
-import com.vaadin.flow.component.Component;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
-
 
 import java.awt.*;
 
+
 @Route("")
 public class MainView extends VerticalLayout {
+    public MainView() {
+        Button button = new Button("Click me!");
+        Button button1 = new Button("Clear");
+        TextField textField = new TextField();
 
-    public MainView(){
-        var button = new Button("Click me!");
-        add(new com.vaadin.flow.component.button.Button());
+        add(new HorizontalLayout(button, textField, button1));
+
+        button.addClickListener(e -> {
+            add(new Paragraph("Hello" + textField.getValue()));
+            textField.clear();
+        });
+
+        button1.addClickListener(e -> {
+            
+        });
     }
 }
